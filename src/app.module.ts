194366bd18600b1c00a2c7e3dcef4defce4ad02a
mihara0320 <masaki@grabcad.com>
2022-07-24 14:07:14 +0300

    empty services

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,9 +3,12 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { NavigationController } from './navigation/navigation.controller';
 
+import { RoutesService } from './providers/routes/routes.service';
+import { AirportsService } from './providers/airports/airports.service';
+
 @Module({
   imports: [],
   controllers: [AppController, NavigationController],
-  providers: [AppService],
+  providers: [AppService, RoutesService, AirportsService],
 })
 export class AppModule {}
